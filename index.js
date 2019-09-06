396d65aa1d3a5426efbfc9b93036efb8f0d6eae0
Tadas Jodwalis <tjodwalis@gmail.com>
2019-09-06 16:24:15 -0500

    Updated gitignore and switched to IGDB api

--- index.js
+++ index.js
@@ -11,17 +11,39 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
 });
 
+// app.get('/pc', (req, res) => {
+//     try {
+//         axios
+//             .get(
+//                 `https://www.giantbomb.com/api/games/?api_key=${process.env.GIANTBOMB_API_KEY}&format=json&platforms=`
+//             )
+//             .then(data => {
+//                 res.status(200).send(data.data);
+//             })
+//             .catch(err => res.send(err));
+//     } catch (err) {
+//         console.error('GG', err);
+//     }
+// });
+
 app.get('/pc', (req, res) => {
-    try {
-        axios
-            .get(`https://www.giantbomb.com/api/games/?api_key=${process.env.GIANTBOMB_API_KEY}&format=json`)
-            .then(data => {
-                res.status(200).send(data.data);
-            })
-            .catch(err => res.send(err));
-    } catch (err) {
-        console.error('GG', err);
-    }
+    axios({
+        url: 'https://api-v3.igdb.com/games',
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'user-key': process.env.IGDB_API_KEY
+        },
+        data: `fields name, release_dates.*, status, first_release_date;
+            where release_dates.platform = (6) & release_dates.y > 2019 & themes != (42);
+            limit 50;`
+    })
+        .then(response => {
+            res.status(200).send(response.data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
 });
 
 app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
